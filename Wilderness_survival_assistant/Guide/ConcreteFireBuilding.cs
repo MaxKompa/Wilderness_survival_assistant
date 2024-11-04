d787b88435b6e4f3e887c2e13caaae7bd5f5e65f
Galagen17 <denisgaluzinskijj@gmail.com>
2024-11-05 01:51:56 +0200

    Add and update files

---
+++ Wilderness_survival_assistant/Guide/ConcreteFireBuilding.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Wilderness_survival_assistant.guid
+{
+    
+    public class ConcreteFireBuilding : FireBuilding
+    {
+       
+    }
+}
---
+++ Wilderness_survival_assistant/Guide/SurvivalGuide.cs
@@ -0,0 +1,14 @@
+using System.Text.Json.Serialization.Metadata;
+
+namespace Wilderness_survival_assistant.guid;
+
+public abstract class SurvivalGuide
+{
+    public virtual void DisplayInfo()
+    {
+        Console.WriteLine("\nSurvival Guide");
+    }
+}
+
+
+
