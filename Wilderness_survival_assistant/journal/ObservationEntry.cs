2f129a0ea5027e9f53b5c5f09d2665f08160cd8c
MaxKompa <max.kompa645@gmail.com>
2024-11-05 01:14:40 +0100

    Add Observation log

---
+++ Wilderness_survival_assistant/journal/ObservationEntry.cs
@@ -0,0 +1,15 @@
+﻿namespace Wilderness_survival_assistant.journal;
+
+using System;
+
+public class ObservationEntry
+{
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+
+    public ObservationEntry(string description, DateTime date)
+    {
+        Description = description;
+        Date = date;
+    }
+}
